71295499c4dd5e1780935b90d1d35ee7a8620ba6
Ben Bishop <bish0p@hotmail.com>
2014-07-12 17:46:47 +0000

    updated main

--- public/js/src/views/collection/activities.js
+++ public/js/src/views/collection/activities.js
@@ -1,11 +1,6 @@
 Application.View.Collection.Activities = Backbone.Marionette.CollectionView.extend({
     className: 'activities',
 
-    childView: Application.View.Activity,
+    childView: Application.View.Activity
 
-    render: function () {
-        var template = Handlebars.compile($('#activities-template').html());
-
-        this.$el.html(template);
-    }
 });
