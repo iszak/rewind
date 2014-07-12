0e0aba5fed5d9ba759d239b5926c2b982f590c93
Iszak Bryan <iszak@karmarama.com>
2014-07-12 15:17:42 +0000

    Load Facebook SDK sync

--- public/js/src/App.js
+++ public/js/src/App.js
@@ -1,7 +1,7 @@
 var Application = new Marionette.Application();
 
 Application.addRegions({mainRegion: '#main'});
- 
+
 Application.View = {
     Layout: {},
     Composite: {},
@@ -20,25 +20,25 @@ Application.Controller = {};
  * Facebook / Parse Init
  */
 Application.addInitializer(function() {
-
     Parse.initialize('tQwka3iy4tadatRg4ZPMmWwiR6W5ChAoMTfSkQbS', 'vPBf8a9VEX4FU4yRz2tsvYhlTnvj8hAuFIhmwZfr');
 
-    window.fbAsyncInit = function() {
-        Parse.FacebookUtils.init({
-            appId      : '1436116333338502', 
-            channelUrl : 'http://digital-memory.parseapp.com/channel.html', 
-            status     : true, 
-            cookie     : true, 
-            xfbml      : true
-        });
-    };
-
+    Parse.FacebookUtils.init({
+        appId      : '1436116333338502',
+        channelUrl : 'http://digital-memory.parseapp.com/channel.html',
+        status     : true,
+        cookie     : true,
+        xfbml      : true
+    });
 });
 
 
+// Router
 Application.addInitializer(function(){
-    // Initialize all your routes
     Application.router = new Application.Router.Default();
+});
+
+// History
+Application.addInitializer(function(){
     Backbone.history.start();
 });
 
